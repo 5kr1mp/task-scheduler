b9a1006df599488e388abdf055b576f0ec41576e
Dewfoams <gabrielle.abela69@gmail.com>
2026-04-04 20:13:50 -0700

    Fix immutable list error by using ArrayList

--- src/App.java
+++ src/App.java
@@ -12,7 +12,7 @@ public class App {
         int n = scan.nextInt();
         
         // buhat ug tasks user input ni
-        Task[] taskArray = new Task[n];
+        ArrayList<Task> taskList = new ArrayList<>();
         for (int i = 0; i < n; i++) {
             System.out.println("\nTask " + (i + 1) + ":");
             System.out.print("  Arrival Time: ");
@@ -23,14 +23,14 @@ public class App {
             int pri = scan.nextInt();
             
             int taskNum = i + 1;
-            taskArray[i] = new Task("P" + taskNum, at, bt, pri);
+            taskList.add(new Task("P" + taskNum, at, bt, pri));
         }
         
         System.out.print("\nEnter time quantum: ");
         int quantum = scan.nextInt();
         
         // pass data
-        RR rr = new RR(quantum, taskArray);
+        RR rr = new RR(quantum, taskList);
         rr.run();
         
         System.out.println("\n=== Round Robin Scheduling Complete ===");
